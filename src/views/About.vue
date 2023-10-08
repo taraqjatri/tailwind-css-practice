95a506dfbfc86fa926deacd4b9f747a17db4eb93
taraqjatri <130284178+taraqjatri@users.noreply.github.com>
2023-10-08 12:20:01 +0600

    bootstraping

---
+++ src/views/About.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <div>asdasd
+      <img src="../assets/images/logo.svg" alt="">
+      hello
+    </div>
+  </div>
+</template>
---
+++ src/views/Index.vue
@@ -0,0 +1,32 @@
+<script setup>
+</script>
+
+<template>
+    <div class="bg-gray-100">
+      <div class="grid lg:grid-cols-2 2xl:grid-cols-5">
+        <div class="px-8 py-12 max-w-md mx-auto sm:max-w-xl lg:max-w-full lg:px-12 lg:py-24 xl:mr-0 2xl:col-span-2">
+          <div class="xl:max-w-xl">
+            <img class="h-10" src="../assets/images/logo.svg" alt="workcation">
+            <img class="mt-6 rounded-lg shadow-xl sm:h-64 sm:w-full sm:object-cover object-center lg:hidden" src="../assets/images/beach-work.jpg" alt="Women workcation on the beach">
+
+            <h1 class="text-2xl font-bold text-gray-900 mt-6 sm:text-4xl lg:text-3xl xl:text-4xl">You can work from anywhere.
+              <br class="hidden lg:inline">
+              <span class="text-purple-400 font">Take advantage of it.</span></h1>
+            <p class="mt-6 text-gray-600 sm:text-xl">
+              The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form.
+            </p>n
+
+            <div class="mt-4 sm:mt-6">
+              <a class="px-4 py-2 bg-indigo-500 rounded-lg text-white shadow-lg uppercase tracking-wider text-sm font-semibold sm:text-base" href="#">
+                Book Your Escape
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div class="hidden relative lg:block 2xl:col-span-3">
+          <img class="absolute inset-0 w-full h-full object-cover object-center" src="../assets/images/beach-work.jpg" alt="Women workstation on the beach">
+        </div>
+      </div>
+    </div>
+</template>
